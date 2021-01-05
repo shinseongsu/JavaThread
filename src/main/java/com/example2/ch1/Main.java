16aba844f0a64910db3531735baf27292a3cf627
Shin seongsu <tlstjdtn321@naver.com>
2021-01-05 23:19:12 +0900

    Good 문자열을 10000회 표시하는 프로그램

---
+++ src/main/java/com/example2/ch1/Main.java
@@ -0,0 +1,12 @@
+package com.example2.ch1;
+
+/**
+ * Good!이라는 문자열을 10000회 표시하는 프로그램입니다.
+ */
+public class Main {
+    public static void main(String[] args) {
+        for(int i = 0 ; i < 10000; i++) {
+            System.out.println("Good!");
+        }
+    }
+}
