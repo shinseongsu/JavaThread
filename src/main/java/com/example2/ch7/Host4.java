4a7f9d643722fd9fa834a1f214dbbd1413f59393
Shin seongsu <tlstjdtn321@naver.com>
2021-01-11 22:46:46 +0900

    Executor 객체를 생성자에서 수취하는 Host 클래스

---
+++ src/main/java/com/example2/ch7/Host4.java
@@ -0,0 +1,28 @@
+package com.example2.ch7;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Host4 {
+    private final Helper helper = new Helper();
+    private final ScheduledExecutorService scheduledExecutorService;
+
+    public Host4(ScheduledExecutorService scheduledExecutorService) {
+        this.scheduledExecutorService = scheduledExecutorService;
+    }
+
+    public void request(final int count, final char c) {
+        System.out.println("    request(" + count + ", " + c + ") BEGIN");
+        scheduledExecutorService.schedule(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        helper.handle(count, c);
+                    }
+                },
+                3L,
+                TimeUnit.SECONDS
+        );
+        System.out.println("    request(" + count + ", " + c + ") END");
+    }
+}
